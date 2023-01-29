e1f9fabc9fce66612366d1cd614571c0625f2c57
PaulKnees <jakewallace368@gmail.com>
2023-01-29 14:24:19 +0000

    save, need to move all inquiries into one variable for proper functionality

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+const managerPrompt = require('./lib/prompts/managerPrompt');
+const engineerPrompt = require('./lib/prompts/engineerPrompt');
+const internPrompt = require('./lib/prompts/internPrompt')
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -14,4 +19,29 @@ const { TestScheduler } = require("rxjs/testing");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-console.log('tester')
+function getData() {
+    const addMenu = {
+        name: 'addMenu',
+        type: 'list',
+        choices: ['Add engineer', 'Add intern', 'Finish building team']
+    }
+    inquirer.prompt(managerPrompt).then((managerData) => {
+        inquirer.prompt(addMenu)
+            .then((addChoice) => {
+                while (addChoice !== 'Finish buildin team') {
+                    if (addChoice === 'Add engineer') {
+                        inquirer.prompt(engineerPrompt).then((engineerData) => {
+                        })
+                        if (addChoice === 'Add intern') {
+                            inquirer.prompt(internPrompt)
+                        }
+
+                    }
+
+
+                }
+            })
+    })
+}
+
+getData()
