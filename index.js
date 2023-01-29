4d79336806affd33aafba09c17b1a0e002a393e1
PaulKnees <jakewallace368@gmail.com>
2023-01-29 21:57:58 +0000

    prompts for all employees + menu

--- index.js
+++ index.js
@@ -15,33 +15,46 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 const { TestScheduler } = require("rxjs/testing");
+const { getEnvironmentData } = require("worker_threads");
+const { get } = require("http");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
-
-function getData() {
-    const addMenu = {
-        name: 'addMenu',
-        type: 'list',
-        choices: ['Add engineer', 'Add intern', 'Finish building team']
-    }
-    inquirer.prompt(managerPrompt).then((managerData) => {
-        inquirer.prompt(addMenu)
-            .then((addChoice) => {
-                while (addChoice !== 'Finish buildin team') {
-                    if (addChoice === 'Add engineer') {
-                        inquirer.prompt(engineerPrompt).then((engineerData) => {
-                        })
-                        if (addChoice === 'Add intern') {
-                            inquirer.prompt(internPrompt)
-                        }
-
-                    }
-
-
-                }
-            })
+const addMenu = {
+    name: 'nextEmployee',
+    type: 'list',
+    choices: ['Add engineer', 'Add intern', 'Finish building team']
+}
+function getManData() {
+    inquirer.prompt(managerPrompt).then((mData) => {
+        const manager = new Manager(mData.name, mData.ID, mData.email, mData.officeNumber)
+        menu()
     })
 }
-
-getData()
+function menu() {
+    inquirer.prompt(addMenu).then((menuData) => {
+        if (menuData.nextEmployee === 'Add engineer') {
+            
+            getEngData()
+        }
+        if (menuData.nextEmployee === 'Add intern') {
+            getIntData()
+        }
+        if (menuData.nextEmployee === 'Finish building team') {
+            return
+        }
+    })
+}
+function getEngData() {
+    inquirer.prompt(engineerPrompt).then((engData) => {
+        const engineer = new Engineer(engData.name, engData.ID, engData.email, engData.github);
+        menu()
+    })
+}
+function getIntData() {
+    inquirer.prompt(internPrompt).then((intData => {
+        const intern = new Intern(intData.name, intData.ID, intData.email, intData.school);
+            menu()
+    }))
+}
+getManData()
