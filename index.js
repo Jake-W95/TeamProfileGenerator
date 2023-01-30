6d9e3ff3f0270965a94c3af70ca7d62afc80489a
PaulKnees <jakewallace368@gmail.com>
2023-01-30 12:13:21 +0000

    file created, added output dir

--- index.js
+++ index.js
@@ -18,17 +18,20 @@ const { TestScheduler } = require("rxjs/testing");
 const { getEnvironmentData } = require("worker_threads");
 const { get } = require("http");
 
-
+const team = []
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 const addMenu = {
     name: 'nextEmployee',
+    message: 'Add another employee?',
     type: 'list',
     choices: ['Add engineer', 'Add intern', 'Finish building team']
 }
 function getManData() {
     inquirer.prompt(managerPrompt).then((mData) => {
         const manager = new Manager(mData.name, mData.ID, mData.email, mData.officeNumber)
+        team.push(manager)
         menu()
+
     })
 }
 function menu() {
@@ -40,20 +43,25 @@ function menu() {
             getIntData()
         }
         if (menuData.nextEmployee === 'Finish building team') {
-            return
+            
+            fs.writeFile(outputPath, render(team), err => {if (err) throw err})
+            
+            
         }
     })
 }
 function getEngData() {
     inquirer.prompt(engineerPrompt).then((engData) => {
         const engineer = new Engineer(engData.name, engData.ID, engData.email, engData.github);
+        team.push(engineer)
         menu()
     })
 }
 function getIntData() {
     inquirer.prompt(internPrompt).then((intData => {
         const intern = new Intern(intData.name, intData.ID, intData.email, intData.school);
-            menu()
+        team.push(intern)
+        menu()
     }))
 }
 getManData()
